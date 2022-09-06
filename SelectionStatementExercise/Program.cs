c2f8483a2cc4d42b2e88c273ac31b935ca44276b
Schwenk-A <100094451+Schwenk-A@users.noreply.github.com>
2022-09-06 13:39:20 -0500

    Created Template

---
+++ SelectionStatementExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelectionStatementExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
